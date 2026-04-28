667495abfa77d33ba2c69d3a9879cf09b1d03eaf
kaushikkumar100x <kaushikkumarprajapati443@gmail.com>
2026-04-28 09:56:16 +0530

    class2

--- class1.py
+++ class1.py
@@ -2,5 +2,45 @@
 # print("codelife")
 
 
-if True:
-    Print("kaushik kumar")
+# if True:
+#     print("kaushik kumar")
+
+# print("codelife100x")
+
+
+
+# inter the user input and print it
+# a = input("enter your name: ")
+# print("hello",a)
+
+   # addtion of two numbers
+# n_1 = int(input("enter first number: "))
+# n_2 = int(input("enter second number: "))
+# print("sum is", n_1 + n_2)
+
+# a = 5
+# A = 10
+# print(A+a)
+# print("the value of a is", a)
+
+# print("""lorem ipsum dolor sit amet, consectetur adipiscing
+#       elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
+#       aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+#       aliquip ex ea commodo consequat. Duis aute irure
+#       dolor in reprehenderit in voluptate velit esse cillum 
+#       dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+#       sunt in culpa qui officia deserunt mollit anim id est laborum.""")
+
+
+
+# a = 10
+# b = 20
+# print("the sum of a and b is", a+b)
+# if True:
+#  print("kaushik kumar")
+
+a = int(input("Enter you age"))
+if a >= 18:
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
---
+++ class2.py
@@ -0,0 +1,47 @@
+#variable and data type 
+
+#1 variable intro
+
+# name = "codeflife100x"  # name ek variable hai,  = ass oper, codelife100x ek value .
+# print(name)  
+
+
+
+# n1 = "Ram"
+# print(n1)
+
+
+#naming rule
+
+# 1num = 10
+# print(1num)
+
+
+# -num = 10
+# print(-num)
+
+# age = 12
+# name_1 = "kaushik kumar"
+# print(age,name_1)
+
+# dynamic type and type() fuction
+
+
+# a = 10
+# b = 1.5
+# c = "codelife100x"
+# d = False
+# print(a,b,c)
+# print(a,type(a))
+# print(b,type(b))
+# print(c,type(c))
+# print(d,type(d))
+
+# type casting
+
+# a = int(input("Enter the number:"))
+# b = int(input("Enter the number:"))
+# print(a,type(a))
+# print(b,type(b))
+# print(a+b)
+# print(a-b)
