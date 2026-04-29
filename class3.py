b3035e1eb3a82dd347df8377ff5f8a5ef5b34b01
kaushikkumar100x <kaushikkumarprajapati443@gmail.com>
2026-04-29 19:29:39 +0530

    class3

---
+++ class3.py
@@ -0,0 +1,45 @@
+# 1.Create a variable named city and assign the value "New York" to it.
+
+# city = "New York"
+# City = "New York100"
+# print(city,City)
+
+# 2. If a = 5 and b = 2, write the code to find the result of a raised to the power of b.
+
+# a = 5
+# b = 2
+# c = a**b
+# print(c)
+
+# 3. Write a line of code to find the length of the string name = "codelife100x".
+
+# a = "codelife100x"
+# print(len(a))
+
+# How do you convert the number 7 into a float?
+
+# a = 7
+# print(float(a))
+# a = 7
+# b = float(a)
+# print(b,type(b))
+
+# 4. Given the string s = "Programming", how do you print only the first 4 letters?
+
+# s = "Programming"
+# print(s[0:4])
+
+
+# 5. Write the code to check the data type of x = 99.9.
+
+# x = 99.9
+
+# print(type(x),x)
+
+# If x = "10" and y = "5", write a code to print the result of x + y as 15 (integer addition).
+x = "10"
+y = "5"
+a = int(x)
+b = int(y)
+print("sum", x+y)
+print("sum", a+b)
