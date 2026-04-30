4ea9d640bc4cc646d94459d07ad4ffb489af61e5
kaushikkumar100x <kaushikkumarprajapati443@gmail.com>
2026-04-30 15:51:21 +0530

    class5

---
+++ class5.py
@@ -0,0 +1,50 @@
+#A op
+# a = 105
+# b = 50
+# print(a+b ,"sum")
+# print(a-b ,"sub")
+# print(a*b ,"mult")
+# print(a%b ,"modl")
+# print(a**b ,"pow")
+# print(a//b ,"flo di")
+# print(a/b ,"div")
+
+
+# comp op
+
+# a = 100
+# b = 20
+# print(a == b, "Equal")
+# print(a > b, "less")
+# print(a < b, "grater")
+# print(a >= b, "less than equal")
+# print(a <= b, "greater than Equal")
+
+#log op
+
+
+# a = 10
+# b = 5
+# print(a>5 and b<10,"And")
+# print(a>5 or b>10,"or")
+# print(not(a>5),"not")
+
+# ass op
+
+# a = 100
+# a += 50 
+# print(a)
+# a -=50
+# print(a)
+# a *= 5
+# print(a)
+
+
+
+# memb op
+
+# name = "codelife100x"
+# print("l" in name )
+# print("x" not in name)
+
+
