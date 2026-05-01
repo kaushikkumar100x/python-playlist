75b52d92b9fa0a5d3376bffc4c0af5dd3173be04
kaushikkumar100x <kaushikkumarprajapati443@gmail.com>
2026-05-01 18:53:40 +0530

    class6

---
+++ class6.py
@@ -0,0 +1,98 @@
+# 1. Perform arithmetic operations
+
+
+# a = 5
+# b = 3
+# print(a+b,a-b,a*b,a/b)
+
+# 2. Find remainder using modulus
+
+# a = 5
+# b = 2
+# print(a%b)
+
+# 3. Use exponent operator
+
+# a = 2
+# b = 4
+
+# print(a**b)
+
+
+# 4. Compare two numbers
+
+# a = 5
+# b = 3
+# print(a>b, b>a, a==b)
+
+# 5. Logical AND operator
+
+# a = 5
+# b = 3
+
+# if(a>1 and b>1):
+#     print("its true")
+
+# 8. Assignment operator
+
+# a = 5
+# a += 10
+# print(a)
+
+# 10. Membership operator 'in'
+
+# a = "kaushik kumar"
+# print("h" in a)
+
+# 12. Identity operator 'is'
+
+# a = "kaushik kumar"
+# b = "shivam pal"
+# print(a is b)
+# print(a is not b)
+
+# 4. Operator precedenc
+
+# a = 10/2*4+4-2
+# print(a)
+
+# 15. Parentheses in precedence
+
+# a = (2+5)*2
+# print(a)
+
+# 16. Arithmetic + comparison
+
+# a = 5
+# b = 3
+# if(a+b) >1:
+#     print("true")
+
+# 19. Even or odd check
+
+# a = int(input("Enter the number :"))
+
+# if(a%2 == 0):
+#     print("Even")
+# else:
+#     print("odd")
+
+# 20. Logical + comparison
+
+# a = int(input("Enter the age :"))
+
+# if(a>18 and a<30):
+#     print("younge")
+# elif(a>60 and a<100):
+#     print("old")
+# else:
+#     print("child")
+
+
+a = 100
+b = 20
+print(a == b, "Equal")
+print(a > b, "less")
+print(a < b, "grater")
+print(a >= b, "less than equal")
+print(a <= b, "greater than Equal")
---
+++ class7.py
@@ -0,0 +1,61 @@
+# a = int(input("Enter the age : "))
+# b = int(input("Enter the second age : "))
+# print(a, type(a))
+# print(b, type(b))
+# print(a + b)
+
+
+# first_name = input("Please Enter your first name :")
+# last_name = input("please Enter your Last name :")
+# print("Welcome to",first_name ,"", last_name)
+
+
+
+# marks = int(input("Enter the marks :"))
+# marks1 = int(input("Enter the marks :"))
+# marks2 = int(input("Enter the marks :"))
+# print(marks+marks1+marks2, type(marks),type(marks1),type(marks2))
+
+
+# marks = float(input("Enter the marks :"))
+# marks1 = float(input("Enter the marks :"))
+# marks2 = float(input("Enter the marks :"))
+# print(marks+marks1+marks2, type(marks),type(marks1),type(marks2))
+
+
+# print("kaushik kumar \n codelife100x")
+# print("kaushik kumar \t codelife100x")
+# print("kaushik kumar \\ codelife100x")
+# print("kaushik kumar \" codelife100x")
+# print("kaushik kumar ' codelife100x")
+# print("shivam pal \r codelife100x")
+# print("shivam pal \b codelife100x")
+# print("shivam pal \f codelife100x")
+# print("shivam pal \v codelife100x")
+# print("shivam pal \a codelife100x")
+# print("shivam ",end="")
+# print("pal")
+
+
+# first_name = input("Please Enter your first name :")
+# last_name = input("please Enter your Last name :")
+# print("Welcome to",first_name ,"", last_name)
+
+# name = "kaushik kumar"
+# age = 18
+# marks = 50
+# fri_name = "shivam pal"
+# print("My name is", name, "and my age is", age, "and my marks are", marks, "and my friend name is", fri_name)
+
+
+# name = "kaushik kumar"
+# age = 18
+# marks = 50
+# fri_name = "shivam pal"
+# print(f"My name is {name}.my age is {age} .my marks are {marks} .my friend name is {fri_name}.")
+
+
+
+a = 5
+b = 10
+print(f"sum of {a} and {b} is {a+b}")
