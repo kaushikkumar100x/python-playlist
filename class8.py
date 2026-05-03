f7de9f89f433daf2c11f182ebd213671c1b76718
kaushikkumar100x <kaushikkumarprajapati443@gmail.com>
2026-05-03 11:12:59 +0530

    class8

---
+++ class8.py
@@ -0,0 +1,99 @@
+# 1. Take input from user and print it
+
+# first_name = input("Enter the first name :")
+# last_name = input("Enter the last name :")
+# print("Welcome","",first_name,"",last_name)
+
+
+# 2. Take integer input and print square
+
+# a = int(input("Enter the number :"))
+# sq = a**2
+# print("Result", sq)
+
+# 3. Take float input and print area of circle
+
+# r = float(input("Enter the radius of circle :"))
+# area = 3.14*r**2
+# print("Area of the circle is :", area)
+
+# 4. Take two numbers and print sum
+
+# a = int(input("Enter a fisrt number :"))
+# b = int(input("Enter the second number :"))
+
+# result = a+b
+# print("the result of this numbers :",result)
+
+# 5. Type casting string to int
+
+# a = "10"
+# b = int(a)
+# print("sum of this numbers",b +10)
+
+
+# 6. Type casting float to int
+
+# a = float(25.25)
+# b = int(a)
+# print(b)
+
+# 7. Print with separator
+
+# print("kaushik","kumar","codelife100x",sep="__")
+
+
+# 8. Print with end parameter
+
+# print("kaushik kumar",end=" ")
+# print("codelife100x")
+
+# 9. Using f-string
+# name = "kaushik kumar "
+# age = 18
+# marks = 56
+# print(f"My name is {name}.my age is {age}.my marks is {marks}")
+
+# 10. Using format()
+
+# name = "kaushik kumar"
+# print(f"my name is {name }")
+# print("My name is {}".format(name))
+
+
+# 11. Escape characters
+
+# print("kaushik \n kumar")
+# print("kaushik \t kumar")
+
+# 12. Print quotes
+# print('my name is "kaushik kumar"')
+
+# 13. Multiple inputs in one line
+
+# a,b = map(int.input("Enter the numbers :")).split()
+# print(a+b)
+
+# a = map(list(int(input("Enter the numbers :")).split()))
+# print(a)
+# 14. Convert string to uppercase
+
+# name = input("Enter the name :")
+# print(name.upper())
+
+
+# 14. Convert string to uppercase
+# name = input("Enter the name :")
+# print(len(name))
+
+# 16. Format float
+# num=3.14159
+# print('Value: %.2f' % num)
+
+
+# 17. Display type of input
+# 18. Convert input to float and add
+
+a = float(input("Enter the number :"))
+b = float(input("Enter the second number :"))
+print(a+b)
