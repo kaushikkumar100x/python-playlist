6f14162f6f71aa805c547aa885229fd577189d6c
kaushikkumar100x <kaushikkumarprajapati443@gmail.com>
2026-05-04 16:56:25 +0530

    class9

---
+++ class9.py
@@ -0,0 +1,64 @@
+# a = "kaushik kumar"
+# b = 'codelife100x'
+# c = """kaushik 
+#         kumar"""
+# d = '''code
+#           life
+#               100x'''
+# print(a ,"\n",b,"\n",c,"\n",d)
+
+
+#indexing string
+
+# a = "kaushik kumar"
+# print(a[-4])
+# print(a[4])
+
+
+# string slicing
+
+# a = "kaushik kumar"
+# print(a[0:5:3])
+
+# a = "1234567890"
+# print(a[::-1])
+
+#string  methods
+# a = "kaushik kumar"
+# print(a.upper())
+# print(a.capitalize())
+# print(a.title())
+# b  = "CODELIFE100X"
+# print(b.lower())
+# print(b.casefold()) 
+# print(a.count("k"))
+# print(a.strip())
+# print(a.lstrip())
+# print(a.rstrip())
+# print(a.find("k"))
+# print(a.index("k"))
+# print(a.replace("k","K"))
+# print(a.split())
+# print(a.split(" "))
+# print(a.startswith("kumar"))
+# print(a.endswith("kaushik"))
+
+
+# con rep
+
+# a = "1"
+# b = "life"
+# c = "100x"
+# print(a+b+c)
+# print(a,b,c)
+# print(a*10)
+
+#strnig formating
+
+# a = "kaushik"
+# b = "kumar"
+# c = 100
+# print("my name is {} {} {}".format(a,b,c))
+# print(f"my name is {a} {b} {c}")
+    
+print('lorem ipsum "\n" dolor sit amet,"\t" consectetur adipiscing"\\" elit, sed do"\'" eiusmod tempor incididunt"\"" ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
