0959c05ebb24b2db54e77400cd808c644ad0d9ed
kaushikkumar100x <kaushikkumarprajapati443@gmail.com>
2026-05-06 09:24:48 +0530

    class11

---
+++ class11.py
@@ -0,0 +1,149 @@
+# if use 
+
+
+# a = int(input("Enter the age :"))
+
+# if(a>=18):
+#     print("you are eligible for vote")
+    
+    
+# a = int(input("Enter the marks :"))
+
+# if(a>=90):
+#     print("you topper",a)
+    
+    
+    
+#if else ka use 
+
+# a = input("Enter the shivam :")
+
+# if(a == "shivam"):
+#     print("your name is shivam")
+# else:
+#     print("your name is not shivam")
+
+
+# a = int(input("Enter the marks :"))
+
+# if(a>=80):
+#     print("you are topper",a)
+# else:
+#     print("you are not topper",a)
+    
+    
+# a = float(input("Enter the your attendance  rate :"))
+
+# if(a>=75):
+#     print("you are eligible for exam",a)
+# else:
+#     print("you are not eligible for exam",a)
+
+
+
+# a = int(input("Enter the marks :"))
+
+# if(a>=90):
+#     print("your grade is A",a)
+# elif(a>=80):
+#     print("your grade is B",a)
+# elif(a>=60):
+#     print("your grade is C",a)          
+# elif(a>=45):
+#     print("your grade is D",a)
+# elif(a>=33):
+#     print("your grade is E",a)  
+# else:
+#     print("your grade is F",a)
+
+
+
+# a = input("Enter you  name :")
+
+# if(a == "shivam"):
+#     print("Bihar",a)
+# elif(a == "kaushik kumar"):
+#     print("BSR",a)
+# elif(a == "farhan"):
+#     print("POK",a)
+# else:
+#     print("your name is not found",a)
+
+
+#nested if statement
+
+# name = input("Enter you  name :")
+# city = input("Enter you city :")
+# a = int(input("Enter the marks :"))
+# if(city == "bihar"):
+#     if(name == "shivam"):
+#         if(a>=90):
+#            print("your grade is A",a)
+#         elif(a>=80):
+#            print("your grade is B",a)
+#         elif(a>=60):
+#             print("your grade is C",a)          
+#         elif(a>=45):
+#             print("your grade is D",a)
+#         elif(a>=33):
+#             print("your grade is E",a)  
+#         else:
+#              print("your grade is F",a)
+
+
+#         print("your name is shivam and your city is bihar",name,city)
+#     else:
+#         print("your name is not shivam but your city is bihar",name,city)
+# else:
+#     print("Your data is not found",name,city)
+
+
+
+
+# a = int(input("Enter the Day number :"))
+
+# match a:
+#     case 1:
+#         print("Monday",a)
+#     case 2:
+#         print("Tuesday",a)
+#     case 3:
+#         print("Wednesday",a)
+#     case 4:
+#         print("Thursday",a)
+#     case 5:
+#         print("Friday",a)
+#     case 6:
+#         print("Saturday",a)
+#     case 7:
+#         print("Sunday",a)
+#     case _:
+#         print("Invalid day number",a)
+    
+    
+# a = int(input("Enter the roll no :"))
+# match a:
+#     case 1:
+#         print("your name is shivam . you are pass")
+#     case 2:
+#         print("your name is farhan . you are fail")
+#     case 4:
+#            print("your name is Ram . you are pass")
+#     case 5:
+#           print("your name is kaushik . you are fail")
+#     case 6:
+#           print("your name is karan . you are pass")
+#     case 7:
+#         print("its not found ")
+        
+        
+# a = int(input("Enter you age :"))
+
+# result = "Adult" if a >=18 else "minner"
+# print(result,a)
+
+
+a = int(input("Enter the marks :"))
+
+marks = "topper" if a>=85 else "middel"
+print(marks , a)
