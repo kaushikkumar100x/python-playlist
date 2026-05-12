48b6855fee5285146f46bb4f069fb63adba14e08
kaushikkumar100x <kaushikkumarprajapati443@gmail.com>
2026-05-12 11:57:29 +0530

    class17

---
+++ class17.py
@@ -0,0 +1,74 @@
+# Square
+
+# for row in range(1,6):
+#     for col in range(1,6):
+#         if(row==1 or row==5):
+#             print("* ",end="")
+#         elif(col==1 or col ==5):
+#             print("* ",end="")
+#         else:
+#             print("  ",end="")
+#     print()
+
+
+# rect
+
+# for row in range(1,4):
+#     for col in range(1,8):
+#         if(row==1 or row==3):
+#             print("* ",end="")
+#         elif(col==1 or col ==7):
+#             print("* ",end="")
+#         else:
+#             print("  ",end="")
+#     print()
+
+# for row in range(1,6):
+#     for col in range(1,6):
+#         if(row ==5 or col==1 or col==row):
+#             print("* ",end="")
+       
+#         else:
+#             print("  ",end="")
+#     print()
+            
+# n = 5
+# for i in range(n):
+#     for j in range(2*n-1):
+#         if(j==n-i-1 or j ==n+i-1 or i==n-1):
+#             print("* ",end="")
+#         else:
+#             print("  ",end="")
+#     print()
+
+# n = 5
+# for i in range(n):
+#     for j in range(n):
+#         if(j==i or j==n-i-1):
+#             print("* ",end="")
+#         else:
+#             print("  ",end="")
+#     print()
+    
+    
+# n =9
+# mid = n//2
+# for i in range(n):
+#     for j in range(n):
+#         if(i==mid or j==mid):
+#             print("* ",end="")
+#         else:
+#             print("  ",end="")
+#     print()
+
+
+# for row in range(1,6):
+#     for col in range(1,6):
+#         if(row==1 or row==5):
+#             print("1 ",end="")
+#         elif(col==1 or col ==5):
+#             print("1 ",end="")
+#         else:
+#             print("  ",end="")
+#     print()
+            
